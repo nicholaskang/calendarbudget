6953da57fe04d5f6d23e97b484be7c3127a064cb
Nicholas Kang <nickkang257@gmail.com>
2015-08-13 16:22:25 -0500

    added seeds file to allow users to have expense/income types and frequencies model set up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@all_income_types = ["Wages", "Salary", "Commission Checks", "Investment Income", "Gifts", "Other Income"]
+@all_income_types.each do |type|
+	IncomeType.create(name: type)
+end
+
+@all_expense_types = ["Rent", "Mortgage", "Charity", "Groceries", "Restaurants", "Pet Food", "Electricity", "Water", "Heating", "Garbage", "Phone", "Cable", "Internet", "Clothes", "Car Payment", "Fuel", "Car Maintenance", "Transportation", "Auto Insurance", "Health Insurance", "Healthcare Costs", "Other Insurance", "Toiletries", "Gym Membership", "Hygiene", "Student Loan", "Personal Loan", "Credit Card Payment", "Retirement Saving", "Investing", "School Tuition", "Entertainment", "Vacation"]
+@all_expense_types.each do |type|
+	ExpenseType.create(name: type)
+end
+
+@all_frequency_types = ["Monthly", "Bi-Monthly", "Weekly", "Daily", "One Time"]
+@all_frequency_types.each do |type|
+	Frequency.create(value: type)
+end
